f167de662c4c3cde28c9ae29bd2e595671afdfee
andy82115 <andy82115@gmail.com>
2024-09-10 13:49:26 +0900

    Initial Commit

---
+++ AndyHexagonWheel/AndyHexagonWheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndyHexagonWheelApp.swift
+//  AndyHexagonWheel
+//
+//  Created by Andy Yeh on 2024/9/10.
+//
+
+import SwiftUI
+
+@main
+struct AndyHexagonWheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AndyHexagonWheel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AndyHexagonWheel
+//
+//  Created by Andy Yeh on 2024/9/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
