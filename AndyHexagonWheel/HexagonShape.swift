fe34706e2285a8ba453c027f2a3349b9bf1c3424
andy82115 <andy82115@gmail.com>
2024-12-02 15:07:29 +0900

    hexagoon animation with swiftUI

---
+++ AndyHexagonWheel/HexagonShape.swift
@@ -0,0 +1,8 @@
+//
+//  HexagonShape.swift
+//  AndyHexagonWheel
+//
+//  Created by Andy Yeh on 2024/9/10.
+//
+
+import Foundation
---
+++ AndyHexagonWheel/HexagonWheel.swift
@@ -0,0 +1,18 @@
+//
+//  HexagonWheel.swift
+//  AndyHexagonWheel
+//
+//  Created by Andy Yeh on 2024/9/10.
+//
+
+import SwiftUI
+
+struct HexagonWheel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HexagonWheel()
+}
